de088010c1e46ea73dfd1e848fc73f0a93862479
Stéphane Bidoul <stephane@dorie.(none)>
2010-11-13 15:03:39 +0100

    Added GPL license

--- PhononClasses.py
+++ PhononClasses.py
@@ -1,3 +1,19 @@
+# This file is part of edledit.
+# Copyright (C) 2010 Stephane Bidoul
+#
+# edledit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# edledit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with edledit.  If not, see <http://www.gnu.org/licenses/>.
+
 from PyQt4.phonon import Phonon
 
 class VideoPlayer(Phonon.VideoPlayer):
