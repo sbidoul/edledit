4869b67a9607fd0e577e9a9be603290bb3df464f
U-EUR\acsesbi <acsesbi@mcacsesbi02.eur.ad.sag>
2010-11-17 11:00:03 +0100

    Set version to 0.9 for first public release

--- edledit/edledit.py
+++ edledit/edledit.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with edledit.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "1.0"
+__version__ = "0.9"
 
 import os, mimetypes
 from datetime import timedelta
